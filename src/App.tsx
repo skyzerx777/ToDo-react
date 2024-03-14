a8e45a722699ac475c337202eda3d456cbc2ecc2
Yevhenii Hrebenshchykov <yevheniihrebenshchykov@gmail.com>
2024-03-14 16:07:40 +0200

    added fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import './App.css';
+import SearchInput from './components/SearchInput';
+
 function App() {
-	return <div>Home</div>;
+	return (
+		<div className='container mx-auto px-1 md:px-10 xl:px-40 text-center pt-7'>
+			<h1 className='uppercase font-bold text-2xl'>Todo list</h1>
+			<SearchInput />
+		</div>
+	);
 }
 
 export default App;
